481e5b05a857b314ea73c6da010ad1f468594776
Anders Kaare <anders@straadt.dk>
2018-06-19 14:06:51 +0200

    `./update-translations.sh`
    ran `./update-translations.sh` which updated `src/traks-translations.js` by
    adding en/da for each <T> tag

--- src/traks-translations.js
+++ src/traks-translations.js
@@ -32,4 +32,110 @@ import { O } from 'traks/o';
  */
 
 export default {
+	"e8c2410dd77c": {
+		"_new": true, // FIXME remove this line when translation is done
+		"_refs": [
+			"src/App.js:39",
+		],
+		"en": (count) => <O>You have {count} unread message(s)</O>,
+		"da": (count) => <O>You have {count} unread message(s)</O>,
+	},
+
+	"cc4198384dca": {
+		"_new": true, // FIXME remove this line when translation is done
+		"_refs": [
+			"src/App.js:48",
+		],
+		"en": () => <O>Simple translation</O>,
+		"da": () => <O>Simple translation</O>,
+	},
+
+	"bb4a048e3afe": {
+		"_new": true, // FIXME remove this line when translation is done
+		"_refs": [
+			"src/App.js:51",
+		],
+		"en": (Comment, name) => <O>Hello, {name}! <Comment>The name, {name}, is dynamic</Comment></O>,
+		"da": (Comment, name) => <O>Hello, {name}! <Comment>The name, {name}, is dynamic</Comment></O>,
+	},
+
+	"3839d7d18601": {
+		"_new": true, // FIXME remove this line when translation is done
+		"_refs": [
+			"src/App.js:58",
+		],
+		"en": () => <O>You can handle singular/plural cases in translation code</O>,
+		"da": () => <O>You can handle singular/plural cases in translation code</O>,
+	},
+
+	"701a43e44be6": {
+		"_new": true, // FIXME remove this line when translation is done
+		"_context": "file",
+		"_refs": [
+			"src/App.js:61",
+		],
+		"en": () => <O>Save</O>,
+		"da": () => <O>Save</O>,
+	},
+
+	"49ef2d15742d": {
+		"_new": true, // FIXME remove this line when translation is done
+		"_context": "money",
+		"_refs": [
+			"src/App.js:63",
+		],
+		"en": () => <O>Save</O>,
+		"da": () => <O>Save</O>,
+	},
+
+	"7ce29ca93eb3": {
+		"_new": true, // FIXME remove this line when translation is done
+		"_context": "soccer",
+		"_refs": [
+			"src/App.js:65",
+		],
+		"en": () => <O>Save</O>,
+		"da": () => <O>Save</O>,
+	},
+
+	"29beb7a14a14": {
+		"_new": true, // FIXME remove this line when translation is done
+		"_context": "jesus",
+		"_refs": [
+			"src/App.js:67",
+		],
+		"en": () => <O>Save</O>,
+		"da": () => <O>Save</O>,
+	},
+
+	"58099e7e5b4e": {
+		"_new": true, // FIXME remove this line when translation is done
+		"_refs": [
+			"src/App.js:70",
+		],
+		"en": () => {
+			return (
+				<O>
+					<div>You</div>
+					<div>can</div>
+					<div>also</div>
+					<div>have</div>
+					<div>multiline</div>
+					<div>translations</div>
+				</O>
+			);
+		},
+		"da": () => {
+			return (
+				<O>
+					<div>You</div>
+					<div>can</div>
+					<div>also</div>
+					<div>have</div>
+					<div>multiline</div>
+					<div>translations</div>
+				</O>
+			);
+		},
+	},
 }
