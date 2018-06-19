ad0313a642c94ccdc17bb3245efd100679c43557
Anders Kaare <anders@straadt.dk>
2018-06-19 14:23:15 +0200

    removing unused import

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Container, Form, FormGroup, InputGroup, Label, Input } from 'reactstrap';
 import './App.css';
 import { T, TraksConsumer } from './traks';
